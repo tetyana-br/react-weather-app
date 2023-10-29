6d7e8c87136bca7c10096bfe6fc88165ad3be2cb
tetyana-br <tnaroditskaya@gmail.com>
2023-10-29 22:37:54 +0200

    Added GitHub url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Tetyana N. and is{" "}
+        <a
+          href="https://github.com/tetyana-br/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
